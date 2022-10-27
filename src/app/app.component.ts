40c0d316231ffb4e3b8ecaedcce8149138409510
Shreedhara, Basanth Maipady basanth.shreedhara@siemens-healthineers.com <basanth.shreedhara@siemens-healthineers.com>
2022-10-27 19:19:02 +0530

    Added updateComplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,51 @@ import {
   ViewContainerRef,
 } from '@angular/core';
 import '../assets/table-container.js';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../styles.scss'],
+  styles: [
+    `
+      .outofstock {
+        font-weight: 700;
+        color: #ff5252;
+        text-decoration: line-through;
+      }
+
+      .lowstock {
+        font-weight: 700;
+        color: #ffa726;
+      }
+
+      .instock {
+        font-weight: 700;
+        color: #66bb6a;
+      }
+
+      :host ::ng-deep .row-accessories {
+        background-color: rgba(0, 0, 0, 0.15) !important;
+      }
+    `,
+  ],
 })
 export class AppComponent implements OnInit {
   @ViewChild('viewContainerRef', { read: ViewContainerRef, static: true })
   public orderedViewContainer: ViewContainerRef;
 
+
   constructor(
     public viewContainerRef: ViewContainerRef,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
-    this.addDynamicFoodChoice();
+    //this.loadTableComponent();
   }
 
-  public async addDynamicFoodChoice(): Promise<void> {
+  public async loadTableComponent(): Promise<void> {
     const { PtableWrapperComponent } = await import(
       './ptable-wrapper/ptable-wrapper.component'
     );
--- src/app/ptable-wrapper/ptable-wrapper.component.ts
+++ src/app/ptable-wrapper/ptable-wrapper.component.ts
@@ -5,20 +5,23 @@ import * as allReportingColumns from '../../assets/reporting-column-option.json'
 @Component({
   selector: 'app-ptable-wrapper',
   templateUrl: './ptable-wrapper.component.html',
-  styleUrls: ['./ptable-wrapper.component.css']
+  styleUrls: ['./ptable-wrapper.component.css'],
 })
 export class PtableWrapperComponent implements OnInit {
   allColumns: any = (allReportingColumns as any).default;
   procedures: any;
   display = false;
   index: number = 1;
-  constructor(private http: HttpClient, private cdRef:ChangeDetectorRef) {}
+  constructor(private http: HttpClient, private cdRef: ChangeDetectorRef) {}
 
   ngOnInit() {
-    this.http.get('../assets/large-dataset.json').subscribe(proc => {
-      console.log(proc);
-      this.procedures = proc;
-      this.display = true;
+    const container = document.querySelector('table-container') as any;
+    container.updateComplete.then(() => {
+      this.http.get('../assets/large-dataset.json').subscribe((proc) => {
+        console.log(proc);
+        this.procedures = proc;
+        this.display = true;
+      });
     });
   }
 }
